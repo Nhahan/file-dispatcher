42c46c49e65bd68c58d6a48068b71d6152df73e8
Nhahan <kisy324@naver.com>
2023-06-28 03:38:49 +0900

    feat: support both cjs & mjs

---
+++ fixup.sh
@@ -0,0 +1,15 @@
+cp dist/mjs/index.d.ts dist
+
+rm -rf dist/*/index.d.ts
+
+cat >dist/cjs/package.json <<!EOF
+{
+    "type": "commonjs"
+}
+!EOF
+
+cat >dist/mjs/package.json <<!EOF
+{
+    "type": "module"
+}
+!EOF
