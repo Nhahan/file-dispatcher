d20e204fd8c3c0c90f66e87858983543ecfe40e3
Nhahan <kisy324@naver.com>
2023-06-28 13:37:48 +0900

    chore: build with webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    clean: true,
+    library: {
+      name: 'FileDispatcher',
+      type: 'umd',
+    },
+    globalObject: 'this',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  target: 'node',
+};
