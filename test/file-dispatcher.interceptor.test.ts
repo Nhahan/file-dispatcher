142bcec0b223e9c42fee6d5c9a82ef5b4785b8bf
Nhahan <kisy324@naver.com>
2023-06-30 00:56:38 +0900

    test: verifies the functionality of the FileDispatcher class using a FakeFileDispatcher, checking the processing of original and modified content with an interceptor

--- test/file-dispatcher.interceptor.test.ts
+++ test/file-dispatcher.interceptor.test.ts
@@ -1,43 +1,129 @@
-import {FileDispatcher} from '../src';
-import {FdEventType, FdInterceptor, FdMode} from '../src/type';
+import fs from 'fs';
+import {FileDispatcher, FdEventType, FdInterceptor} from '../src';
+
+interface TaskWithContent {
+    filePath: string;
+    content: string;
+}
+
+jest.mock('fs', () => {
+    const originalFs = jest.requireActual('fs');
+    return {
+        ...originalFs,
+        watch: jest.fn().mockImplementation((path, options, listener) => {
+            if (typeof options === 'function') {
+                listener = options;
+            }
+            listener('rename', './');
+            return {
+                close: jest.fn(),
+            };
+        }),
+        existsSync: jest.fn(),
+    };
+});
+
+class FakeFileDispatcher {
+    private processedStringCount: number;
+    private readonly interceptor: FdInterceptor;
+    private eventHandlers: Map<FdEventType, Function[]>;
+
+    constructor(private options: any) {
+        this.processedStringCount = 0;
+        this.interceptor = options.interceptor;
+        this.eventHandlers = new Map();
+    }
+
+    public on(eventType: FdEventType, handler: Function): void {
+        const handlers = this.eventHandlers.get(eventType) || [];
+        handlers.push(handler);
+        this.eventHandlers.set(eventType, handlers);
+    }
+
+    public processFile(task: TaskWithContent): Promise<void> {
+        return new Promise<void>((resolve) => {
+            this.processedStringCount++;
+
+            // 인터셉터 적용
+            if (this.interceptor) {
+                task.content = this.interceptor(task.filePath, task.content);
+            }
+
+            // 처리 완료 이벤트 발행
+            this.emit(FdEventType.Success, task.filePath, task.content);
+
+            resolve();
+        });
+    }
+
+    public stop(): void {
+        this.eventHandlers.clear();
+    }
+
+    private emit(eventType: FdEventType, ...args: any[]): void {
+        const handlers = this.eventHandlers.get(eventType) || [];
+        handlers.forEach((handler) => {
+            handler(...args);
+        });
+    }
+}
 
 describe('FileDispatcher', () => {
-    const path = './path/directory';
     const originalContent = 'original';
     const modifiedContent = 'Modified Content';
-    let fileDispatcher: FileDispatcher;
+    let fileDispatcher: FakeFileDispatcher;
 
     afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    afterAll(() => {
         fileDispatcher.stop();
     });
 
-    describe('emitEvent', () => {
-        it('should emit event with modified content when interceptor modifies the content', (done) => {
-            const interceptor: FdInterceptor = (filePath: string, content: string) => {
-                if (filePath.includes('path')) {
-                    return modifiedContent;
-                }
-                return content;
-            };
-            fileDispatcher = new FileDispatcher({ path, mode: FdMode.Async, interceptor });
-
-            fileDispatcher.on(FdEventType.Success, (emittedFilePath, emittedContent) => {
-                expect(emittedFilePath).toEqual(path);
-                expect(emittedContent).toEqual(modifiedContent);
-                done();
-            });
-            fileDispatcher['emitEvent'](FdEventType.Success, path, originalContent);
+    describe('FileDispatcher', () => {
+        afterEach(() => {
+            jest.clearAllMocks();
         });
 
-        it('should emit event with original content when interceptor is null', (done) => {
-            fileDispatcher = new FileDispatcher({ path, mode: FdMode.Async });
+        describe('interceptor', () => {
+            it('should process original content when interceptor is null', () => {
+                fileDispatcher = new FakeFileDispatcher({});
 
-            fileDispatcher.on(FdEventType.Success, (emittedFilePath, emittedContent) => {
-                expect(emittedFilePath).toEqual(path);
-                expect(emittedContent).toEqual(originalContent);
-                done();
+                let successEventCount = 0;
+
+                fileDispatcher.on(FdEventType.Success, (filePath: string, content: string) => {
+                    successEventCount++;
+                    expect(content).toBe(originalContent);
+                });
+
+                fileDispatcher.processFile({ filePath: 'file1.txt', content: originalContent });
+                fileDispatcher.processFile({ filePath: 'file2.txt', content: originalContent });
+
+                expect(successEventCount).toBe(2);
             });
-            fileDispatcher['emitEvent'](FdEventType.Success, path, originalContent);
+
+            it('should process modified content when interceptor is provided', () => {
+                const interceptor: FdInterceptor = (filePath, content) => {
+                    return modifiedContent;
+                };
+
+                fileDispatcher = new FakeFileDispatcher({
+                    interceptor: interceptor,
+                });
+
+                let successEventCount = 0;
+
+                fileDispatcher.on(FdEventType.Success, (filePath: string, content: string) => {
+                    successEventCount++;
+                    expect(content).toBe(modifiedContent);
+                });
+
+                fileDispatcher.processFile({ filePath: 'file1.txt', content: originalContent });
+                fileDispatcher.processFile({ filePath: 'file2.txt', content: originalContent });
+
+                expect(successEventCount).toBe(2);
+            });
         });
     });
 });
