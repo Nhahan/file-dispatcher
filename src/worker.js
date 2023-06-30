6154fb006da2ea4763e8c32bfad06ff02e17906a
Nhahan <kisy324@naver.com>
2023-07-01 02:17:46 +0900

    refactor: change interceptor logic to be performed in worker threads instead of the main thread

--- src/worker.js
+++ src/worker.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const {parentPort} = require("worker_threads");
 
-parentPort?.on('message', async ({ filePath }) => {
+parentPort?.on('message', async ({ task, interceptor }) => {
   try {
-    const content = await fs.promises.readFile(filePath, 'utf8');
-    parentPort?.postMessage({ filePath, content });
+    let content = await fs.promises.readFile(task.filePath, 'utf8');
+    if (interceptor) content = interceptor(task.filePath, content);
+    parentPort?.postMessage({ filePath: task.filePath, content });
   } catch (error) {
     console.error('Worker file processing error:', error);
   }
