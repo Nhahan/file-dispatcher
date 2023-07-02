b1f7b73ffd9e562c211cf76d9fd3f322caa06a86
Nhahan <kisy324@naver.com>
2023-07-02 22:42:16 +0900

    fix: copy the cc build output to async and sync

--- build_filemod.sh
+++ build_filemod.sh
@@ -7,5 +7,7 @@ cd ./native
 node-gyp configure
 node-gyp build
 
-mv ./build/Release/filemod.node $current_dir/src/
+cp ./build/Release/filemod.node $current_dir/src/filemod-sync.node
+cp ./build/Release/filemod.node $current_dir/src/filemod-async.node
+
 cd $current_dir
