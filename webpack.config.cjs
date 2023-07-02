ddbb74940e1ee3c78f1f11262c89af2aa0260736
Nhahan <kisy324@naver.com>
2023-07-02 22:39:51 +0900

    docs: error fix / additional webpack settings for native addons

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,4 +1,8 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const FILEMOD_ASYNC_PATH = './src/filemod-async.node';
+const FILEMOD_SYNC_PATH = './src/filemod-sync.node';
 
 module.exports = {
   mode: 'production',
@@ -33,4 +37,12 @@ module.exports = {
     ],
   },
   target: 'node',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: FILEMOD_ASYNC_PATH, to: 'filemod-async.node' },
+        { from: FILEMOD_SYNC_PATH, to: 'filemod-sync.node' },
+      ],
+    }),
+  ],
 };
