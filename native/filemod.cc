2fe277b758a14714bb28c0ee8218496e0024ee4d
Nhahan <kisy324@naver.com>
2023-07-02 19:56:50 +0900

    faet: added support for other operating systems.

--- native/filemod.cc
+++ native/filemod.cc
@@ -4,10 +4,16 @@
 #include <fstream>
 #include <sstream>
 
+#ifdef _WIN32
+  #include <windows.h>
+#else
+  #include <sys/stat.h>
+#endif
+
 bool FetchFileTimesAndContent(const std::string& filePath, std::string& fileContent);
 bool CompareFileTimes(const timespec& creationTime, const timespec& lastWriteTime);
 
-std::string NewFileContent(const std::string& filePath) {
+std::string GetNewFileContent(const std::string& filePath) {
   std::string fileContent;
   if (FetchFileTimesAndContent(filePath, fileContent)) {
     return fileContent;
@@ -16,10 +22,23 @@ std::string NewFileContent(const std::string& filePath) {
 }
 
 #ifdef _WIN32
-  #include <windows.h>
+  HANDLE OpenAndLockFile(const std::string& filePath) {
+    HANDLE hFile = CreateFile(filePath.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile != INVALID_HANDLE_VALUE) {
+      if (LockFile(hFile, 0, 0, MAXDWORD, MAXDWORD)) {
+        return hFile;
+      }
+    }
+    return INVALID_HANDLE_VALUE;
+  }
 
-  HANDLE OpenAndLockFile(const std::string& filePath);
-  bool GetFileTimes(HANDLE hFile, FILETIME& creationTime, FILETIME& lastWriteTime);
+  bool GetFileTimes(HANDLE hFile, FILETIME& creationTime, FILETIME& lastWriteTime) {
+    if (GetFileTime(hFile, &creationTime, NULL, &lastWriteTime)) {
+      return true;
+    }
+    return false;
+  }
+
   bool FetchFileTimesAndContent(const std::string& filePath, std::string& fileContent) {
     FILETIME creationTime, lastWriteTime;
     HANDLE hFile = OpenAndLockFile(filePath);
@@ -39,6 +58,7 @@ std::string NewFileContent(const std::string& filePath) {
           strStream << inFile.rdbuf();
           fileContent = strStream.str();
           inFile.close();
+          CloseHandle(hFile);
           return true;
         }
       }
@@ -48,9 +68,13 @@ std::string NewFileContent(const std::string& filePath) {
   }
 
 #elif defined(__APPLE__)
-  #include <sys/stat.h>
+  bool CompareFileTimes(const timespec& creationTime, const timespec& lastWriteTime) {
+    if (creationTime.tv_sec == lastWriteTime.tv_sec && creationTime.tv_nsec == lastWriteTime.tv_nsec) {
+      return true;
+    }
+    return false;
+  }
 
-  bool CompareFileTimes(const timespec& creationTime, const timespec& lastWriteTime);
   bool FetchFileTimesAndContent(const std::string& filePath, std::string& fileContent) {
     struct stat fileStat;
     if (stat(filePath.c_str(), &fileStat) == 0) {
@@ -67,9 +91,13 @@ std::string NewFileContent(const std::string& filePath) {
   }
 
 #else
-  #include <sys/stat.h>
+  bool CompareFileTimes(const time_t& creationTime, const time_t& lastWriteTime) {
+    if (creationTime == lastWriteTime) {
+      return true;
+    }
+    return false;
+  }
 
-  bool CompareFileTimes(const time_t& creationTime, const time_t& lastWriteTime);
   bool FetchFileTimesAndContent(const std::string& filePath, std::string& fileContent) {
     struct stat fileStat;
     if (stat(filePath.c_str(), &fileStat) == 0) {
@@ -87,7 +115,7 @@ std::string NewFileContent(const std::string& filePath) {
 
 #endif
 
-void NewFileContent(const v8::FunctionCallbackInfo<v8::Value>& args) {
+void Node_NewFileContent(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
 
   if (args.Length() < 1 || !args[0]->IsString()) {
@@ -99,24 +127,13 @@ void NewFileContent(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::String::Utf8Value filename(isolate, args[0]);
   std::string filePath(*filename);
 
-  std::string fileContent = NewFileContent(filePath);
+  std::string fileContent = GetNewFileContent(filePath);
 
   args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, fileContent.c_str()).ToLocalChecked());
 }
 
-void Initialize(v8::Local<v8::Object> module) {
-  v8::Local<v8::Object> exports = v8::Object::New(module->GetIsolate());
-
-  NODE_SET_METHOD(exports, "newFileContent", NewFileContent);
-
-  auto maybeSet = module->Set(
-      module->GetIsolate()->GetCurrentContext(),
-      v8::String::NewFromUtf8(module->GetIsolate(), "exports").ToLocalChecked(),
-      exports);
-
-  if (maybeSet.IsNothing()) {
-    std::cerr << "Failed to set module exports." << std::endl;
-  }
+void Initialize(v8::Local<v8::Object> exports, v8::Local<v8::Value> module) {
+  NODE_SET_METHOD(exports, "newFileContent", Node_NewFileContent);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
--- src/filemode.d.ts
+++
@@ -1,5 +0,0 @@
-// filemod.node.d.ts
-
-declare module 'filemod.node' {
-    export function newFileContent(filePath: string): string | null;
-}
