d3172ece9175e4017305e27b0553ebda1a12625c
Nhahan <kisy324@naver.com>
2023-07-03 00:31:33 +0900

    docs: change the build process to only compile 'index.ts' and copy the rest of the files

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,14 +1,9 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const FILEMOD_ASYNC_PATH = './src/filemod-async.node';
-const FILEMOD_SYNC_PATH = './src/filemod-sync.node';
 
 module.exports = {
   mode: 'production',
   entry: {
     index: './src/index.ts',
-    worker: './src/worker.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -21,7 +16,7 @@ module.exports = {
     globalObject: 'this',
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js', '.node']
   },
   module: {
     rules: [
@@ -30,19 +25,7 @@ module.exports = {
         use: ['babel-loader', 'ts-loader'],
         exclude: /node_modules/,
       },
-      {
-        test: /\.node$/,
-        loader: 'node-loader',
-      },
     ],
   },
   target: 'node',
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: FILEMOD_ASYNC_PATH, to: 'filemod-async.node' },
-        { from: FILEMOD_SYNC_PATH, to: 'filemod-sync.node' },
-      ],
-    }),
-  ],
 };
