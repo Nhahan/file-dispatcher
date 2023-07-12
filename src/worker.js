07b7eb6a9f5a7e946d862317b9ad801ffa2ac28f
Nhahan <kisy324@naver.com>
2023-07-12 23:05:29 +0900

    fix: logical relocation of .node files

--- src/worker.js
+++ src/worker.js
@@ -3,12 +3,8 @@ const path = require("path");
 
 parentPort?.on('message', async (filePath) => {
   try {
-    const filemodPath = workerData.i ?
-        path.resolve(__dirname, 'filemod-sync.node') :
-        path.resolve(__dirname, 'filemod-async.node');
-
+    const filemodPath = workerData.i ? path.resolve(__dirname, 'filemod-async.node') : path.resolve(__dirname, 'filemod-sync.node');
     const filemod = require(filemodPath);
-
     const content = filemod.newFileContent(filePath);
     content && parentPort?.postMessage({ filePath, content });
   } catch (error) {
